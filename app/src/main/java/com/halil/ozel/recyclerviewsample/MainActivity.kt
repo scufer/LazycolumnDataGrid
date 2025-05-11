5b89a618202faba0df7fda1cb8a43f4ba1946768
Halil Ozel <halilozel1903@gmail.com>
2025-05-11 13:51:59 +0300

    Unused import deleted.

--- app/src/main/java/com/halil/ozel/recyclerviewsample/MainActivity.kt
+++ app/src/main/java/com/halil/ozel/recyclerviewsample/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
